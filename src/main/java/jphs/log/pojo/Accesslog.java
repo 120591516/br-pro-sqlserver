d60132d2c8c8f0b49beff06b6003ec4af5c38b6f
120591516 <120591516@qq.com>
2017-06-05 21:18:41 +0800

    sa

--- src/main/java/jphs/log/pojo/Accesslog.java
+++ src/main/java/jphs/log/pojo/Accesslog.java
@@ -1,86 +1,97 @@
 package jphs.log.pojo;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Accesslog implements Serializable {
-	private Integer id;
+    private static SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-	private Integer productId;
+    private static SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
 
-	private Integer uv;
+    private Integer id;
 
-	private Integer pv;
+    private Integer productId;
 
-	private Date accesstime;
+    private Integer uv;
 
-	private Date starttime;
+    private Integer pv;
 
-	private Date endtime;
+    private Date accesstime;
 
-	private Integer platformId;
+    private Date starttime;
 
-	public Integer getId() {
-		return id;
-	}
+    private Date endtime;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    private Integer platformId;
 
-	public Integer getProductId() {
-		return productId;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setProductId(Integer productId) {
-		this.productId = productId;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public Integer getUv() {
-		return uv;
-	}
+    public Integer getProductId() {
+        return productId;
+    }
 
-	public void setUv(Integer uv) {
-		this.uv = uv;
-	}
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
 
-	public Integer getPv() {
-		return pv;
-	}
+    public Integer getUv() {
+        return uv;
+    }
 
-	public void setPv(Integer pv) {
-		this.pv = pv;
-	}
+    public void setUv(Integer uv) {
+        this.uv = uv;
+    }
 
-	public Date getAccesstime() {
-		return accesstime;
-	}
+    public Integer getPv() {
+        return pv;
+    }
 
-	public void setAccesstime(Date accesstime) {
-		this.accesstime = accesstime;
-	}
+    public void setPv(Integer pv) {
+        this.pv = pv;
+    }
 
-	public Date getStarttime() {
-		return starttime;
-	}
+    public Date getAccesstime() {
+        return accesstime;
+    }
 
-	public void setStarttime(Date starttime) {
-		this.starttime = starttime;
-	}
+    public void setAccesstime(Date accesstime) {
+        this.accesstime = accesstime;
+    }
 
-	public Date getEndtime() {
-		return endtime;
-	}
+    public Date getStarttime() {
+        return starttime;
+    }
 
-	public void setEndtime(Date endtime) {
-		this.endtime = endtime;
-	}
+    public void setStarttime(Date starttime) {
+        this.starttime = starttime;
+    }
 
-	public Integer getPlatformId() {
-		return platformId;
-	}
+    public Date getEndtime() {
+        return endtime;
+    }
 
-	public void setPlatformId(Integer platformId) {
-		this.platformId = platformId;
-	}
+    public void setEndtime(Date endtime) {
+        this.endtime = endtime;
+    }
+
+    public Integer getPlatformId() {
+        return platformId;
+    }
+
+    public void setPlatformId(Integer platformId) {
+        this.platformId = platformId;
+    }
+
+    @Override
+    public String toString() {
+        return this.productId + "\tpv值是" + this.pv + "\tuv值是" + this.uv + "\t访问日期" + dayFormat.format(accesstime)
+                + "\t开始时间" + timeFormat.format(starttime) + "\t结束时间" + timeFormat.format(endtime);
+    }
 }
